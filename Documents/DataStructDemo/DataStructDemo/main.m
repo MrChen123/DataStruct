2059b1c26180016edee6e6aeae4fde28bac46b23
MrChen123 <289659801@qq.com>
2019-12-25 14:45:30 +0800

    first Commit

---
+++ Documents/DataStructDemo/DataStructDemo/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  DataStructDemo
+//
+//  Created by 信辉科技 on 2019/12/24.
+//  Copyright © 2019年 信辉科技. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Status.h"
+#import "Scanf.h"
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
